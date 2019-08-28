a822f7b77434a003be4b396674f173dea35aef03
Samyak Jain <samyak.jain@calsofthq.com>
2019-08-28 17:13:41 +0530

    first commit

---
+++ src/main/java/com/samyak/recipe/recipe/RecipeApplication.java
@@ -0,0 +1,14 @@
+package com.samyak.recipe.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeApplication.class, args);
+	}
+
+}
+
